9daa6acf233d956eb116c2414cd6110e97da5f26
Charlie Greene <greeneca@gmail.com>
2012-07-14 22:03:19 -0300

    Update locations

--- torrent_dl.sh
+++ torrent_dl.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-/home/greenot/Scripts/torrents/pytvshows/pytvshows --output_dir=/home/greenot/Dropbox/Torrents
+
+/home/greenot/Scripts/torrents/pytvshows/pytvshows -v --output_dir=/home/greenot/Dropbox/Torrents >> /home/greenot/Logs/pytvshows.log
