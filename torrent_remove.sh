c76eeba7ecad473cdf3473e37134901ce9cfa2a9
Charlie Greene <greeneca@gmail.com>
2012-12-01 16:54:25 -0400

    fix log

--- torrent_remove.sh
+++ torrent_remove.sh
@@ -4,7 +4,7 @@ LIST=`transmission-remote -n "greenot:a new grey hen" --list | grep "100%" | awk
 
 for ID in $LIST
 do
-    NAME=transmission-remote -n "greenot:a new grey hen" --torrent 652 --info | grep Name | awk '{print $2}'
+    NAME=`transmission-remote -n "greenot:a new grey hen" --torrent 652 --info | grep Name | awk '{print $2}'`
     echo "[`date`] Removing $NAME from transmission" >> /home/greenot/Logs/torrent.log
     transmission-remote -n "greenot:a new grey hen" --torrent $ID --remove
 done
