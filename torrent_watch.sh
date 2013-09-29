703948aa215bdb01c772b9e786c623b1e3a0c695
Charlie Greene <greeneca@gmail.com>
2013-09-29 18:27:15 -0300

    update flexget

--- torrent_watch.sh
+++ torrent_watch.sh
@@ -4,7 +4,7 @@ for file in /home/greenot/Dropbox/Torrents/*.torrent
 do
     if [ "$file" != "/home/greenot/Dropbox/Torrents/*.torrent" ]; then
         echo "[`date`] Adding $file to transmission" >> /home/greenot/logs/torrent.log
-        /usr/bin/transmission-remote -n 'greenot:ind5NKSfSXtA' -a "$file" && rm "$file"
+        /usr/bin/transmission-remote -n 'greenot:a new grey hen' -a "$file" && rm "$file"
         sleep 1
     fi
 done
